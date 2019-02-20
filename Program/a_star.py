1465478ad5eeebb028be4212f16c375d1c48f299
Mohammad Hooshdar <mohammad.hooshdar@yahoo.com>
2019-02-21 01:00:20 +0330

    a star for program implementation added

---
+++ Program/a_star.py
@@ -0,0 +1,47 @@
+def aStar(problem, isGraph):
+    startState = problem.initialState()
+    f = list()
+    fWeight = list()
+    e = list()
+    nodePath = list()
+    tempState = startState
+
+    expandedNodes = 0
+    seenNodes = 0
+    maxH = 0
+
+    try:
+        f.append(startState)
+        seenNodes += 1
+        fWeight.append(problem.heuristic(startState))
+        nodePath.append([startState])
+    except:
+        print "invalid start state"
+
+    while f:
+        minWeightIndex = fWeight.index(min(fWeight))
+        tempState = problem.result(tempState, f.pop(minWeightIndex))
+        tempCost = fWeight.pop(minWeightIndex) - problem.heuristic(tempState)
+        tempPath = nodePath.pop(minWeightIndex)
+        if problem.goalTest(tempState):
+            return {"seen": seenNodes, "expanded": expandedNodes, "route": tempPath, "cost": tempCost,
+                    "max memory": maxH}
+        if isGraph:
+            e.append(tempState)
+        expandedNodes += 1
+        neighbors = problem.actions(tempState)
+        for node in neighbors:
+            if (isGraph and (node not in e)) or (not isGraph):
+                cost = problem.pathCost(tempState, node) + problem.heuristic(node)
+                tempList = list(tempPath)
+                tempList.append(node)
+                if node in f:
+                    if cost + tempCost < fWeight[f.index(node)]:
+                        fWeight[f.index(node)] = cost + tempCost
+                        nodePath[f.index(node)] = tempList
+                else:
+                    f.append(node)
+                    seenNodes += 1
+                    fWeight.append(cost + tempCost)
+                    nodePath.append(tempList)
+        maxH = max(maxH, len(f))

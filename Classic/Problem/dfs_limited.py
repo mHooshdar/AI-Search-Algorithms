fd020f1318a6cf99ce7077d3c6125f7552438fa5
Mohammad Hooshdar <mohammad.hooshdar@yahoo.com>
2019-02-25 00:04:03 +0330

    dfs limited added for problem

---
+++ Classic/Problem/dfs_limited.py
@@ -0,0 +1,42 @@
+def dfsLimited(problem, l, isGraph):
+    f = list()
+    e = list()
+    startState = problem.initialState()
+    nodePath = list()
+
+    expandedNodes = 0
+    seenNodes = 0
+    cost = 0
+    maxH = 0
+    tempState = startState
+
+    try:
+        f.append(startState)
+        seenNodes += 1
+        nodePath.append([startState])
+    except:
+        print "invalid start state"
+
+    while f:
+        tempState = problem.result(tempState, f.pop(0))
+        tempPath = nodePath.pop(0)
+        if len(tempPath) >= l:
+            continue
+        if isGraph:
+            e.append(tempState)
+        expandedNodes += 1
+        neighbors = problem.actions(tempState)
+        count = 0
+        for node in neighbors:
+            if (isGraph and (node not in e and node not in f)) or (not isGraph):
+                tempList = list(tempPath)
+                tempList.append(node)
+                f.insert(0 + count, node)
+                seenNodes += 1
+                nodePath.insert(0 + count, tempList)
+                count += 1
+                if problem.goalTest(node):
+                    maxH = max(maxH, len(f))
+                    return {"seen": seenNodes, "expanded": expandedNodes, "route": tempList, "cost": cost,
+                            "max memory": maxH}
+        maxH = max(maxH, len(f))
